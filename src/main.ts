84fe74bbc6b8d99c23c1c4ef04dd1f5ed5463e53
Facundo Castro <123614305+schweigenderFlugel@users.noreply.github.com>
2024-06-08 14:53:45 -0300

    add port as environment variable

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const port = process.env.PORT || 3000;
+
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
