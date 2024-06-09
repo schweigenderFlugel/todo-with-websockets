a70b6b082ba81fe60cf6c96a4057e6c3bd2a3444
Facundo Castro <123614305+schweigenderFlugel@users.noreply.github.com>
2024-06-09 13:26:13 -0300

    Update user.interface.ts

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,9 +1,11 @@
 export interface IUser {
+  username: string;
   email: string;
   password: string;
 }
 
 export interface IUserModel {
   getUserByEmail(email: IUser['email']);
+  getUserByUsername(username: IUser['username'];
   createUser(user: IUser): Promise<string>;
 }
