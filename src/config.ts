2504f0447e7fa19568a8fec1f579e447db9a2793
schweigenderFlugel <facundoezcastro@gmail.com>
2024-08-02 03:20:09 -0300

    add modifications to .env and config file

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,11 @@ export default registerAs('config', () => {
     mongodbUri: process.env.MONGODB_URI,
 
     jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
+    jwtAccessExpiration: process.env.JWT_ACCESS_EXPIRATION,
+
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+    jwtRefreshExpiration: process.env.JWT_REFRESH_EXPIRATION,
+
     cookieName: process.env.COOKIE_NAME,
 
     cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
